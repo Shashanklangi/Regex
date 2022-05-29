b65c17fddca38f5b7a85ffda8be0994a3fd4b74a
Shashank <langishashank@gmail.com>
2022-05-29 21:22:15 +0530

    [Shashank]:Validating Password Rule 1.

--- Regex/Regex/Validation.cs
+++ Regex/Regex/Validation.cs
@@ -13,6 +13,7 @@ namespace RegexPattern
         public string LastName = ("^[A-Z]{1}[A-Za-z]{3,}$");
         public string Email = ("^[A-Za-z]{3,}@[a-z]{3,}.[a-z]{3,}$");
         public string PhoneNumber = ("^[A-Za-z]{3,}@[a-z]{3,}.[a-z]{3,}$");
+        public string PassWord = ("^[A-z0-9a-z@#&*+]{8,}$");
 
         public void ValidateFirstName(string firstName)
         {
@@ -41,6 +42,13 @@ namespace RegexPattern
                 Console.WriteLine("Valid Phone_Number");
             else
                 Console.WriteLine("Invalid Phone_Number");
+        }
+        public void Password(string Password)
+        {
+            if (Regex.IsMatch(Password,PassWord))
+                Console.WriteLine("Valid Password");
+            else
+                Console.WriteLine("Invalid Password");
         }
     }
 }
