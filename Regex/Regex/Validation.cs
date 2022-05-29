f490c3473e5f0b11e23c4125a490117e474a3fd9
Shashank <langishashank@gmail.com>
2022-05-29 21:14:48 +0530

    [Shashank]:Validating PhoneNumber.

--- Regex/Regex/Validation.cs
+++ Regex/Regex/Validation.cs
@@ -12,6 +12,7 @@ namespace RegexPattern
         public string FirstName = ("^[A-Z]{1}[A-Za-z]{3,}$");
         public string LastName = ("^[A-Z]{1}[A-Za-z]{3,}$");
         public string Email = ("^[A-Za-z]{3,}@[a-z]{3,}.[a-z]{3,}$");
+        public string PhoneNumber = ("^[A-Za-z]{3,}@[a-z]{3,}.[a-z]{3,}$");
 
         public void ValidateFirstName(string firstName)
         {
@@ -33,6 +34,13 @@ namespace RegexPattern
                 Console.WriteLine("Valid Email_ID");
             else
                 Console.WriteLine("Invalid Email_ID");
+        }
+        public void Phonenumber(string Phone_Number)
+        {
+            if (Regex.IsMatch(Phone_Number, PhoneNumber))
+                Console.WriteLine("Valid Phone_Number");
+            else
+                Console.WriteLine("Invalid Phone_Number");
         }
     }
 }
